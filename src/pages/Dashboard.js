5bcf0d8a590ffff3215afbd3bfc3426f5c19c434
HenriethC.N <areathy33@gmail.com>
2019-12-21 16:10:44 +0100

    Addition of javaScript to shuffle status

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,4 +1,4 @@
-import React  from 'react';
+import React,{ useState}   from 'react';
 import { Header, Sidebar } from './Resuables'
 import GraphOutlineImage from '../images/graph-outline.png';
 import GraphImage from '../images/graph.png';
@@ -27,9 +27,9 @@ const Graphstat = () => (
       <div className='card-body'>
         <div className='row'>
           <div className='col-md-6'>
-            <h5>Today: 5, Aug 2018</h5>
+            <h6>Today: 5, Aug 2018</h6>
           </div>
-          <div className='col-md-6'>
+          <div className='col-md-6 text-right'>
             <select disabled className='graph-select'>
               <option>1 Jan - 1 Jun</option>
             </select>
@@ -69,7 +69,97 @@ const Orderspayment = ({title, title1, title2, total}) => (
     </div>
 )
 
+const Pymt = ({status, name}) => (
+  <React.Fragment>
+    <tr>
+      <td>
+        <div className='item'>VW</div>
+        <span>Apple Mac Book 15" 250 SSD 12GB</span>
+      </td>
+      <td>$73430</td>
+      <td>1234567890</td>
+      <td>12:30</td>
+<td><div className={status}>{name}</div></td>
+      <td><div className='arrow'></div></td>
+    </tr>
+  </React.Fragment>
+)
+
+const TableHeader = ({shuffle}) => (
+  <React.Fragment>
+  <div className='row mb-1 mt-4'>
+    <div className='col-sm-12'><h3>Payments</h3></div>
+  </div>
+  <div className='row my-4' style={{fontSize:'13px'}}>
+  <div className='col-sm-4'>
+    <div>
+      <span>Showing </span>
+      <select>
+        <option>20</option>
+      </select>
+      <span> out of 500 payments</span>
+    </div>
+  </div>
+  <div className='col-sm-4'>
+    <div className='search-payments'>
+      <i className='fa fa-search'></i>
+      <input type='text' placeholder='Search payments'></input>
+    </div>
+  </div>
+  <div className='col-sm-4'>
+    <div>
+      <span>Show</span>
+      <select className='drop-down' onChange={shuffle}>
+        <option className='drop-down-option'>All</option>
+        <option className='drop-down-option'>Reconciled</option>
+        <option className='drop-down-option'>Un-reconciled</option>
+        <option className='drop-down-option'>Settled</option>
+        <option className='drop-down-option'>Unsettled</option>
+      </select>
+    </div>
+  </div>
+  </div>
+  </React.Fragment>
+)
+
+const TableFooter=()=>(
+  <div className='row table-footer'>
+    <div className='col-sm-6'>
+      <span>Showinf 1 to 10 of 500 entries</span>
+    </div>
+    <div className='col-sm-6 text-right'>
+      <ul>
+      <li>Previous</li>
+        <li className='active'>1</li>
+        <li>2</li>
+        <li>Next</li>
+      </ul>
+    </div>
+
+  </div>
+)
+
 export default()=>{
+  const record = ['Pending', 'Reconciled', 'Un-reconciled', 'Pending', 'Reconciled', 'Un-reconciled', 'Pending', 'Reconciled', 'Un-reconciled', 'Pending', 'Reconciled', 'Un-reconciled', 'Pending', 'Reconciled', 'Un-reconciled']
+  const [displayStatus, setdisplayStatus] = useState(record)
+  const [Notfound, setNotfound] = useState(null)
+
+
+
+  let showStatus =  displayStatus.map((value, i)=>
+    <Pymt key={i} status={value==='Pending' ? 'status yellow' : value==='Reconciled' ? 'status green':'status gray' } name={value} />
+  )
+
+  const shovelStatus = (e)=>{
+    let status = e.target.value
+    if(status==='All')
+    return setdisplayStatus(record);
+    const newDisplay = record.filter(newStatus=>status===newStatus)
+    if(newDisplay.length===0)
+    return setNotfound(true)
+    setdisplayStatus(newDisplay)
+  }
+
   return (
     <div>
       <Header />
@@ -83,24 +173,43 @@ export default()=>{
               <Tranctioncard title='Daily Transaction Value' value='4,000,000' />
               </div>
             </div>
-            <div className='col-md-6'>
+            <div className='col-md-6 npr'>
             <div className='row'>
               <Tranctioncard title='Total Transaction Volume' value='452,000' />
               <Tranctioncard title='Total Transaction value' value='4,000,000' /> 
               </div>             
             </div>
           </div>
-          <div className='row mt-4'>
-            <div className='col-md-8 bg-white plr'>
-              <Graphstat />
+            <div className='row mt-4'>
+              <div className='col-md-8 bg-white plr'>
+                <Graphstat />
+              </div>
+              <div className='col-md-4 plr'>
+                <Orderspayment title='Orders' title1='Pending Orders:' title2='Reconciled Orders:' total='Total Orders:' />
+                <Orderspayment title='Payments' title1='Un-reconciled Payments' title2='Reconciledm Payments:' total='Total Payments:' />
+              </div>
             </div>
-            <div className='col-md-4'>
-              <Orderspayment title='Orders' title1='Pending Orders:' title2='Reconciled Orders:' total='Total Orders:' />
-              <Orderspayment title='Payments' title1='Un-reconciled Payments' title2='Reconciledm Payments:' total='Total Payments:' />
+              <TableHeader shuffle={shovelStatus}/>
+            <div className='pymt row mt-2'>
+              <table>
+                <thead>
+                  <tr>
+                    <th>Item Type</th>
+                    <th>Price</th>
+                    <th>Transaction No</th>
+                    <th>Time</th>
+                    <th>Status</th>
+                    <th></th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {Notfound ? <h5 className='text-center'>No Result Found</h5> : showStatus}
+                </tbody>
+              </table>
             </div>
-          </div>
-          </div>
+            <TableFooter />
         </div>
-        </div>
+      </div>
+    </div>
   );
 }
